1c3a7972ea525b43e08e40d085d4ba2bcfb639e6
Katie <katie.bautista@gmail.com>
2016-04-28 09:41:26 -0400

    first commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+  before_action :all_tasks, only: [:index, :create]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  # index and show actions removed
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    #@task = Task.new(task_params)
+    @task  = Task.create(task_params)
+  end
+
+  private
+
+    # new action
+    def all_tasks
+      @tasks = Task.all
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:description, :deadline)
+    end
+end
