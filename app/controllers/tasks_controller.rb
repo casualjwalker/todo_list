f259d19fa03a0f3ff2423d10ca4b5e3442fedafc
Katie <katie.bautista@gmail.com>
2016-04-28 11:08:42 -0400

    added datepicker/sass gems and styling

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :all_tasks, only: [:index, :create]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :all_tasks, only: [:index, :create, :update, :destroy]
+  before_action :set_task, only: [:edit, :update, :destroy, :destroy]
 
   # index and show actions removed
 
@@ -9,13 +9,19 @@ class TasksController < ApplicationController
   end
 
   def create
-    #@task = Task.new(task_params)
     @task  = Task.create(task_params)
+  end
+
+  def update
+     @task.update(task_params) 
+  end
+
+  def destroy
+    @task.destroy
   end
 
   private
 
-    # new action
     def all_tasks
       @tasks = Task.all
     end
